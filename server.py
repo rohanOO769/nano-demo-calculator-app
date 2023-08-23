0e27d20aaf97d150d73e8559082ef8596d72e250
Rohan Amin <aminrohan54@gmail.com>
2023-08-23 21:43:02 +0530

    completed functions

--- server.py
+++ server.py
@@ -1,19 +1,36 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    if request.headers['Content-Type'] != 'application/json':
+        return 'Content-Type must be application/json', 400
+    
+    data = request.json
+    first = data.get('first')
+    second = data.get('second')
+    if first is None or second is None:
+        return 'Both "first" and "second" numbers are required in the request body', 400
+    result = first + second
+    return jsonify({"result": result}), 200
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    if request.headers['Content-Type'] != 'application/json':
+        return 'Content-Type must be application/json', 400
+    
+    data = request.json
+    first = data.get('first')
+    second = data.get('second')
+    if first is None or second is None:
+        return 'Both "first" and "second" numbers are required in the request body', 400
+    result = first - second
+    return jsonify({"result": result}), 200
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(port=8080)
